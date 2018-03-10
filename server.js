cfb0599e1eca6cc7584e36e83408f1bed6174561
Zach Aries <zach.j.aries@gmail.com>
2018-03-09 23:35:53 -0700

    updated ip for deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 app.use ('/public', express.static('public'));
 
